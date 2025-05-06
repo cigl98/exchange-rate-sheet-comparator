d4477bbdcd7234026ddf9201891e881c422385fc
Ondrej Cihlar <ondrej.cihlar@bistudio.com>
2025-05-06 22:40:24 +0200

    Added: empty spring boot project

---
+++ exchange-rate-sheet-comparator/src/test/java/exchangeratesheetcomparator/exchangeratesheetcomparator/ExchangeRateSheetComparatorApplicationTests.java
@@ -0,0 +1,14 @@
+package exchangeratesheetcomparator.exchangeratesheetcomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ExchangeRateSheetComparatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
